67119dc5f37cb1997f78a0f0cf19372fcf301aee
Russ Safin <russ.safin@gmail.com>
2022-02-07 23:37:00 -0800

    Grid generation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
-	Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -10,7 +9,7 @@ const render = Render.create({
 	element: document.body,
 	engine: engine,
 	options: {
-        wireframes: false,
+		wireframes: true,
 		width: width,
 		height: height,
 	},
@@ -18,44 +17,19 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-	world,
-	MouseConstraint.create(engine, {
-		mouse: Mouse.create(render.canvas),
-	})
-);
-
-// const shape = Bodies.rectangle(200, 200, 50, 50, {
-//     isStatic: true
-// });
-// World.add(world, shape);
-
 // Walls
 const walls = [
-	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-	Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-	Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 World.add(world, walls);
 
-// Random Shapes
+// Maze generation
 
-for (let i = 0; i < 40; i++) {
-    if (Math.random() > 0.5) {
-        World.add(
-            world,
-            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-        );
-    } else {
-        World.add(
-            world,
-            Bodies.circle(Math.random() * width, Math.random() * height, 30, {
-                render: {
-                    fillStyle: 'olive'
-                }
-            })
-        );
-    }
-	
-}
+const grid = Array(3)
+	.fill(null)
+	.map(() => Array(3).fill(false));
+
+console.log(grid);
